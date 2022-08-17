a4ef81cadc3d36223444a9c354aef0f287f964a5
Shafi Uddin <2.polash@gmail.com>
2022-08-16 22:38:54 -0400

    First commit

---
+++ src/main/java/BrowserStack/Building_a_selenium_project/App.java
@@ -0,0 +1,13 @@
+package BrowserStack.Building_a_selenium_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
